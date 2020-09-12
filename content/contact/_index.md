f8b59cd0f0411761e54fec014c69b83477e20003
Ram Dettmer <ramdettmer751@gmail.com>
2020-09-11 20:16:15 -0700

    first commit

---
+++ content/contact/_index.md
@@ -0,0 +1,8 @@
++++
+contact_content = "We look forward to connecting with you."
+meta_description = "Contact Anita Iswanto at 714-262-3501 or email anitaeiswanto@gmail.com at Century 21 Garland Realty"
+title = "Contact Me"
+title_tag = "Contact Anita Iswanto - Century 21 Garland Realty"
+url = ""
+
++++
---
+++ static/js/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+
+    // NAVBAR
+    $('.nav-menu').click(function(){
+        $('.nav').toggleClass('nav-open');
+      });
+
+      // Toggle Sort by Price
+      $('.ascending').click(function(){
+        $('.ascend').addClass('hidden');
+        $('.descend').removeClass('hidden');
+      });
+
+      $('.descending').click(function(){
+        $('.descend').addClass('hidden');
+        $('.ascend').removeClass('hidden');
+      });
+});
+
+$('.home-slide').slick({
+  infinite: true,
+  autoplay: true,
+  speed: 1000,
+  autoplaySpeed: 5000,
+});
+
+// TESTIMONIAL SLIDER
+$('.responsive-testimonial-slide').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: true,
+  pauseOnHover: true,
+});
+
+// SINGLE LISTING SLIDER
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+  adaptiveHeight: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true
+});
+
+// LIGHTBOX
+$('.slider-for').slickLightbox();
+
+$('.responsive-slide').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  autoplay: true,
+  slidesToScroll: 3,
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
